4f1508f76b08c3f4f21ec9cf5380624f19682210
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-01 22:16:16 +0000

    added link so each item in array from data goes to new page, the component journalpage

--- expat_journal/src/components/JournalList.js
+++ expat_journal/src/components/JournalList.js
@@ -1,58 +1,49 @@
 import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { getAllJournals } from './actions/actionCreators';
-
-//import all redux, connect function
+import { getAllJournals } from "./actions/actionCreators";
 
 //Need button taking to AddJournal so can add new post!!!!
 
 function JournalList(props) {
+  const history = useHistory();
 
-    const history = useHistory();
+  useEffect(() => {
+    props.getAllJournals();
+  }, []);
 
-    //useEffect to call my props.actionfunction in 
-
-    useEffect(() => {
-        props.getAllJournals()
-    }, [])
-
-    const onLogout = e => {
-        localStorage.removeItem("token");
-        history.push("/login");
-      };
-
-    return (
-        <div>
-            <h4> Your Page of All Your Journal Posts </h4>
-            {props.journals.map(item => (
-                
-                <div>
-                    <p>First Name: {item.first_name}</p>
-                    <p> Last Name: {item.last_name}</p>
-                    <p> Location: {item.location}</p>
-                    <p> Message: {item.message}</p>
-                </div>
-
-            ))}
-
-
-            {/* //Properties from data: caption, first_name, image_url, last_name, location, message */}
-            {/* //Link to indivdual posts for JournalPage */}
-            {/* //REACT ROUTER 2 GUIDED: ITEM LIST style */}
-
-            <button onClick={onLogout}>Log out</button>
-        </div>
-    )
-}
-
-
-const mapStateToProps = state => {
-    return {
-        journals: state.journalListReducer.journals
-    };
+  const onLogout = e => {
+    localStorage.removeItem("token");
+    history.push("/login");
   };
 
+  return (
+    <div>
+      <h4> Your Page of All Your Journal Posts </h4>
+      {props.journals.map(item => (
+        <Link to={`/journallist/${item.id}`} key={item.id}>
+          <div>
+            <p>First Name: {item.first_name}</p>
+            <p> Last Name: {item.last_name}</p>
+            <p> Location: {item.location}</p>
+            <p> Message: {item.message}</p>
+          </div>
+        </Link>
+      ))}
+
+      {/* //Properties from data: caption, first_name, image_url, last_name, location, message */}
+      {/* //Link to indivdual posts for JournalPage */}
+      {/* //REACT ROUTER 2 GUIDED: ITEM LIST style */}
+
+      <button onClick={onLogout}>Log out</button>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    journals: state.journalListReducer.journals
+  };
+};
+
 export default connect(mapStateToProps, { getAllJournals })(JournalList);
-
-
--- expat_journal/src/components/JournalPage.js
+++ expat_journal/src/components/JournalPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { connect } from "react-redux";
 
 //Properties from data: caption, first_name, image_url, last_name, location, message
 
@@ -8,7 +10,7 @@ import React from "react";
 
 // })
 
-function JournalPage() {
+function JournalPage(props) {
   return (
     <div>
       <h3> Your Post: </h3>
