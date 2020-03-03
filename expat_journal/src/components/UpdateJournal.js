02b9b282043bf96334f8424bf8aa5bc1aa6b65a5
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-03 09:35:38 +0000

    styles to all forms the same

--- expat_journal/src/components/UpdateJournal.js
+++ expat_journal/src/components/UpdateJournal.js
@@ -5,6 +5,30 @@ import {
   changeUpdateJournal
 } from "./actions/actionCreators";
 import { connect } from "react-redux";
+import styled from "styled-components";
+
+const Input = styled.input`
+  padding: 0.5rem;
+  width: 50%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #1488cc;
+  border-radius: 3px;
+`;
+
+const Button = styled.button`
+  box-shadow: inset 0px 1px 0px 0px #84e2f3;
+  background: linear-gradient(to bottom, #0d82bd 5%, #0d3cbd 100%);
+  background-color: #1488cc;
+  border-radius: 6px;
+  border: 1px solid #1488cc;
+  color: #ffffff;
+  font-size: 15px;
+  font-weight: bold;
+  padding: 6px 24px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #1488cc;
+`;
 
 function UpdateJournal(props) {
   const history = useHistory();
@@ -33,8 +57,8 @@ function UpdateJournal(props) {
       <form onSubmit={onSubmit}>
         <label>
           {" "}
-          Message:
-          <input
+          Message: <br></br>
+          <Input
             type="text"
             name="message"
             placeholder="Update message"
@@ -45,8 +69,8 @@ function UpdateJournal(props) {
         <br></br>
         <label>
           {" "}
-          Location:
-          <input
+          Location: <br></br>
+          <Input
             type="text"
             name="location"
             placeholder="Update location"
@@ -54,8 +78,9 @@ function UpdateJournal(props) {
             onChange={updateChange}
           />
         </label>
+        <br></br>
 
-        <button> Update </button>
+        <Button> Update </Button>
       </form>
     </div>
   );
