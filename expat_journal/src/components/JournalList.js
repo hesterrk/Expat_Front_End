601ea58ae94455ae8add4661470ed6ab269a3f6a
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-04 19:01:49 +0000

    styling

--- expat_journal/src/components/JournalList.js
+++ expat_journal/src/components/JournalList.js
@@ -60,10 +60,18 @@ const SpecialButton = styled(Button)`
 const H2 = styled.h2`
   display: flex;
   flex-direction: flex-start;
-  margin-left: 160px;
+  margin-left: 50px;
   margin-bottom: 10px;
   padding: 0.4rem;
-  color: black;
+  color: #4b6cb7;
+  margin-top: 0px;
+  text-shadow: 1px 0px 1px #ccc, 0px 1px 1px #eee, 
+    2px 1px 1px #ccc, 1px 2px 1px #eee,
+    3px 2px 1px #ccc, 2px 3px 1px #eee,
+    4px 3px 1px #ccc, 3px 4px 1px #eee,
+    5px 4px 1px #ccc, 4px 5px 1px #eee,
+    6px 5px 1px #ccc, 5px 6px 1px #eee,
+    7px 6px 1px #ccc;
 `;
 
 function JournalList(props) {
@@ -80,7 +88,7 @@ function JournalList(props) {
 
   return (
     <BigDiv>
-      <H2> Expat Journal </H2>
+      <H2> Expat Journal Feed </H2>
       <Link to="/addjournal">
         <Button>Add New Journal</Button>
       </Link>
